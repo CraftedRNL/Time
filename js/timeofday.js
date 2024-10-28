44eab0fe5848ddcc7bf24316683a5a7101054ba8
CraftedRNL <126Rliu@frhsd.com>
2024-10-28 12:49:14 -0400

    Morning maybe finished

--- js/timeofday.js
+++ js/timeofday.js
@@ -1,15 +1,16 @@
 const d = new Date();
 let hour = d.getHours();
 console.log(hour);
+hour = 1;
 if(hour < 12){
-    document.getElementById("greeting").innerText = "Good Mornin";
-    document.getElementById("image").src = "img/GM.jpg"
+    document.getElementById("greeting").innerText = "GET UP YA BUM!";
+    document.getElementById("image").src = "img/SUN.png";
 } else if ( hour < 17){
-    document.getElementById("greeting").innerText = "Good Afternoon";
-    document.getElementById("image").src = "img/GA.jpg"
+    document.getElementById("greeting").innerText = "Its all good.";
+    document.getElementById("image").src = "img/SUN.png";
 } else {
-    document.getElementById("greeting").innerText = "Good Night";
-    document.getElementById("image").src = "img/GN.jpg"
+    document.getElementById("greeting").innerText = "ZZZZZZZZZZ";
+    document.getElementById("image").src = "img/SUN.png";
 }
 
 
